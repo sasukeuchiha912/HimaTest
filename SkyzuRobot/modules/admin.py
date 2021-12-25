b10c51a831bc0bec6b15a238695640a6b59c06f3
Arsh <87128542+ArshCypherZ@users.noreply.github.com>
2021-12-25 12:16:07 +0530

    Update admin.py

--- SkyzuRobot/modules/admin.py
+++ SkyzuRobot/modules/admin.py
@@ -839,7 +839,7 @@ def promote_button(update: Update, context: CallbackContext):
      bot: Optional[Bot] = context.bot
      mode = query.data.split("_")[1]
    
-     try:
+        try:
         if is_user_admin(chat, user.id):
             if mode == "demote":
                 user_id = query.data.split("_")[2]
